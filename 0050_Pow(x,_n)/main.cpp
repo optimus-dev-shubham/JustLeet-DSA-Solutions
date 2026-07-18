37f579d9390e3510acfca731b4bd9e35d10a2e82
Shubham Yadav <contact.shubham1124@gmail.com>
2026-07-18 23:13:48 +0530

    feat(problem): add 0050 Pow(x, n) (Medium)

---
+++ 0050_Pow(x,_n)/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        if (n == 0)
+        return 1;
+            
+        if (n == 1) {
+            return x;
+        }
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+
+        } else
+            return half * half * x;
+    }
+};
