04ed72c4a4eed6d07757221d1e198b78c249855c
Shubham Yadav <contact.shubham1124@gmail.com>
2026-07-27 01:52:49 +0530

    feat(problem): add 0628 Maximum Product of Three Numbers (Easy)

--- 0628_Maximum_Product_of_Three_Numbers/main.cpp
+++ 0628_Maximum_Product_of_Three_Numbers/main.cpp
@@ -3,7 +3,13 @@ public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        
-        return nums[n-1]*nums[n-2]*nums[n-3];
+        if (nums[n - 1] < 0) {
+            return nums[n - 1] * nums[n - 2] * nums[n - 3];
+        } else {
+            return nums[n - 1] * nums[n - 2] * nums[n - 3] >
+                           nums[0] * nums[1] * nums[n - 1]
+                       ? nums[n - 1] * nums[n - 2] * nums[n - 3]
+                       : nums[0] * nums[1] * nums[n - 1];
+        }
     }
 };
