ac98189da266cc6a0cec2e6fd2f9ae50788930e2
Shubham Yadav <contact.shubham1124@gmail.com>
2026-07-27 19:10:07 +0530

    feat(problem): add 1464 Maximum Product of Two Elements in an Array (Easy)

--- 1464_Maximum_Product_of_Two_Elements_in_an_Array/main.cpp
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array/main.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
+        int max = INT_MIN;
+        int smax = INT_MIN;
 
-        return (nums[n-1]-1)*(nums[n-2]-1);
+        for(int num :nums){
+            if(num>=max){
+                smax=max;
+                max=num;
+            }else if(num<max && num>=smax){
+                smax=num;
+            }
+        }
+        return (max-1)*(smax-1);
     }
 };
